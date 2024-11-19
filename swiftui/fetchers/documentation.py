8abcec08968ca0cb449fdd8275ec539021b6c059
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-19 11:52:16 -0500

    Update documentation.py
    - Add `time` package
    - Add `requests` package
    - Create `get_doc` function to handle making the requests and handling the response

--- swiftui/fetchers/documentation.py
+++ swiftui/fetchers/documentation.py
@@ -3,6 +3,8 @@ import sys
 sys.dont_write_bytecode = True
 
 import os
+import time
+import requests
 from dotenv import load_dotenv
 from requests.auth import HTTPProxyAuth
 
@@ -21,5 +23,19 @@ proxies = {
 auth = HTTPProxyAuth(proxy_username, proxy_password)
 
 class DocumentationFetcher:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        
+    def get_doc(self, path):
+        clean_path = path.lstrip('/') + '.json'
+        url = f"{self.config.base_url}{self.config.docs_endpoint}{clean_path}"
+        
+        time.sleep(self.config.request_delay)
+        
+        if self.config.use_proxy == True:
+            response = requests.get(url, proxies=proxies, auth=auth)
+        else:
+            response = requests.get(url)
+            
+        response.raise_for_status()
+        return response.json()
