bbcf733ce3ec81a008ad917f68ea8b74a9fd9f33
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-19 11:50:47 -0500

    Update documentation.py
    - Add `os` package
    - Add `dotenv` package
    - Add `requests` auth package
    - Add proxy vars
    - Create proxy format for use with `HTTPProxyAuth`

--- swiftui/fetchers/documentation.py
+++ swiftui/fetchers/documentation.py
@@ -2,6 +2,24 @@
 import sys
 sys.dont_write_bytecode = True
 
+import os
+from dotenv import load_dotenv
+from requests.auth import HTTPProxyAuth
+
+load_dotenv()
+
+proxy_host = os.getenv('PROXY_HOST')
+proxy_port = os.getenv('PROXY_PORT')
+proxy_username = os.getenv('PROXY_USERNAME')
+proxy_password = os.getenv('PROXY_PASSWORD')
+
+proxies = {
+    'http': f'http://{proxy_username}:{proxy_password}@{proxy_host}:{proxy_port}',
+    'https': f'http://{proxy_username}:{proxy_password}@{proxy_host}:{proxy_port}',
+}
+
+auth = HTTPProxyAuth(proxy_username, proxy_password)
+
 class DocumentationFetcher:
     def __init__(self):
         pass
