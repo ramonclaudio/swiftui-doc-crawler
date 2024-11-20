4336c334e97e99f750526a126a59da70de8a284a
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-20 10:09:38 -0500

    Update client.py
    - Import `dotenv` package
    - Load the `env` file
    - Initialize the config

--- swiftui/services/client.py
+++ swiftui/services/client.py
@@ -2,6 +2,10 @@
 import sys
 sys.dont_write_bytecode = True
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
