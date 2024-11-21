37cea26628c0dde0bb742661be37cb18019e99e3
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-21 13:05:00 -0500

    Update markdown_formatter.py
    - Initialize config

--- swiftui/formatters/markdown_formatter.py
+++ swiftui/formatters/markdown_formatter.py
@@ -3,5 +3,7 @@ import sys
 sys.dont_write_bytecode = True
 
 class MarkdownFormatter:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        self.output_dir = config.output_dir
+        self.data_dir = config.data_dir
