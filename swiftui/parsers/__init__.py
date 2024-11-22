8d8b487ba559ba62a3d46b6934f955ce66f8fc1c
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-22 08:26:21 -0500

    Update __init__.py
    - Initialize `DocumentParser`
    - Initialize `EndpointParser`
    - Initialize `MetadataParser`

--- swiftui/parsers/__init__.py
+++ swiftui/parsers/__init__.py
@@ -1,3 +1,9 @@
 # Prevent caching of bytecode
 import sys
 sys.dont_write_bytecode = True
+
+from .document_parser import DocumentParser
+from .endpoint_parser import EndpointParser
+from .metadata_parser import MetadataParser
+
+__all__ = ['DocumentParser', 'EndpointParser', 'MetadataParser']
