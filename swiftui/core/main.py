e77eebae5be4e60a6cedfc03a436c5579cff181f
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-24 01:15:25 -0500

    Update main.py
    - Import `pathlib` package
    - Import and initialize `Config`
    - Import and initialize `Client`
    - Import and initialize `DocumentParser`
    - Import and initialize `EndpointParser`
    - Import and initialize `MetadataParser`
    - Import and initialize `MarkdownFormatter`
    - Create `data` directory if it doesn't exist
    - Create `output` directory if it doesn't exist

--- swiftui/core/main.py
+++ swiftui/core/main.py
@@ -2,6 +2,24 @@
 import sys
 sys.dont_write_bytecode = True
 
+from pathlib import Path
+
+from ..config.config import Config
+from ..services.client import Client
+from ..parsers.document_parser import DocumentParser
+from ..parsers.endpoint_parser import EndpointParser
+from ..parsers.metadata_parser import MetadataParser
+from ..formatters.markdown_formatter import MarkdownFormatter
+
 class Main:
-    def __init__(self):
-        pass
+    def __init__(self, config_path="swiftui/config/default_config.yaml"):
+        self.config = Config(config_path)
+        self.client = Client(self.config)
+        self.formatter = MarkdownFormatter(self.config)
+        self.endpoint_parser = EndpointParser()
+        self.document_parser = DocumentParser()
+        self.metadata_parser = MetadataParser(self.config.processed_endpoint_file)
+        self.response = None
+        
+        Path(self.config.output_dir).mkdir(parents=True, exist_ok=True)
+        Path(self.config.data_dir).mkdir(parents=True, exist_ok=True)
