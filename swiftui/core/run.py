5b2abb982a66b6c267abcb9b698d79b944d33748
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-11-25 12:38:01 -0500

    Update run.py
    - Create `_run` function to handle running the operation

--- swiftui/core/run.py
+++ swiftui/core/run.py
@@ -7,3 +7,8 @@ from .main import Main
 class Run:
     def __init__(self):
         self.crawler = Main()
+
+    def _run(self, operation_name, *args, **kwargs):
+        operation = getattr(self.crawler, operation_name)
+        response = operation(*args, **kwargs)
+        return self.crawler.get_response()
